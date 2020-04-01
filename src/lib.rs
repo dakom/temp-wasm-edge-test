e9d955485b48624701c86bfe7cc32251bbb8d3a0
David Komer <david.komer@gmail.com>
2020-04-01 23:15:11 +0300

    simplify

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,18 @@
-use wasm_bindgen::prelude::*;
-use dominator::{append_dom, body, html};
+use wasm_bindgen::{prelude::*, JsCast};
+use web_sys::HtmlElement;
 
 #[wasm_bindgen(start)]
 pub fn main_js() {
-    append_dom(&body(), html!("h1", {
-        .text("Hello World!")
-    }));
+    let root_element:HtmlElement = 
+        web_sys::window()
+            .unwrap()
+            .document()
+            .unwrap()
+            .get_element_by_id("web-sys-elem")
+            .unwrap()
+            .dyn_into()
+            .unwrap();
+
+    root_element.set_inner_text("Hello World!");
+
 }
